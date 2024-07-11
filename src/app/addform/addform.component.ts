200fc59f4e71a49dab53a92ee51e35ceaaf30212
Yahyamehar <147187179+Yahyamehar@users.noreply.github.com>
2024-07-11 14:01:37 +0100

    validation added

--- src/app/addform/addform.component.ts
+++ src/app/addform/addform.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import {
   RouterOutlet,
   RouterLink,
   RouterLinkActive,
   Router,
 } from '@angular/router';
+
 
 @Component({
   selector: 'app-addform',
@@ -17,15 +18,28 @@ import {
     RouterLinkActive,
     CommonModule,
     FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './addform.component.html',
   styleUrl: './addform.component.css',
 })
 export class AddformComponent {
-  constructor(private router: Router) {}
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+      membership: ['', Validators.required]
+    });
+  }
 
   onSubmit() {
-    console.log('form submitted');
-    this.router.navigate(['/formsubmit']);
+    if (this.form.valid) {
+      this.router.navigate(['/formsubmit']);
+    } else {
+      this.form.markAllAsTouched();
+    }
   }
 }
